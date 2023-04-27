2281d11d6963a91fa72de3cb9cb84eb4bc2063e3
git-savio1.domi <techsavvydominic3@gmail.com>
2023-04-27 16:24:46 +0300

    Co

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, {useState } from "react";
+import CategoryFilter from "./CategoryFilter";
+import NewTaskForm from "./NewTaskForm";
+import TaskList from "./TaskList";
 
-function NewTaskForm() {
+import { CATEGORIES, TASKS } from "../data";
+console.log("Here's the data you're working with");
+console.log({ CATEGORIES, TASKS });
+
+function App() {
+
+  const [categories, setCategories] = useState(CATEGORIES)
+  const [task, setTask] = useState(TASKS)
+  const [categoryBtn, setCategoryBtn]= useState("All")
+
+  // useEffect(()=>{
+  //   setCategory(TASK.map(task=>task.category))
+  //   console.log(category)
+  // }
+
+  // )
+
+   function addNewItem(newListItem){
+    setTask(...task,newListItem)
+   }
+
+   function itemDelete(itemDelete){
+    setTask(task.filter((item)=>item.text !== itemDelete))
+   }
+
+  const displayedItem = task
+  .filter(
+    (item)=>{
+      if(categoryBtn === "All"){
+        return true
+      }else{
+        return categoryBtn === item.category
+      }
+    }
+
+  )
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
+    <div className="App">
+      <h2>My tasks</h2>
+      <CategoryFilter  selectBtn={setCategoryBtn} onBtn={categoryBtn}  categories={categories} />
+      <NewTaskForm onTaskFormSubmit={addNewItem} categories={categories}/>
+      <TaskList tasks={displayedItem} itemDelete={itemDelete}/>
+    </div>
   );
 }
 
-export default NewTaskForm;
+export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,56 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks, itemDelete}) {
+
+  // const task =function(){
+  //   TASKS.map((task)=> task.text)
+  // }
+
+  // const categories = function(){
+  //   TASKS.map((cat)=>cat.category)
+  // }
+
+  // const [task,setTask]= useState("")
+  // const [taskCategory,setCategory]= useState("All")
+
+
+  // const filteredTaskList = todo?.filter((todos)=>{
+  //   if(onGoingBtn==="All"){
+  //     return true
+  //   }else{
+  //     return(onGoingBtn===todos.category)
+  //   }
+  // })
+
+  // function  itemDelete(text){
+  //   setTodo(todo.filter((todos)=>(todos.text !== text)))
+  //   return todo
+  // }
+
+  // const taskList = filteredTaskList?.map((todo)=>(
+  //   <Task
+  //   key={todo.text}
+  //   text={todo.text}
+  //   category={todo.categorry}
+  //   deleteTodo={deleteTodo}
+  //   />
+  // ))
+
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      
+        {tasks.map((task,index)=>(
+          <Task
+          itemDelete={itemDelete}
+          key={index}
+          text={task.text}
+          category={task.category}
+          />
+        ))}
     </div>
   );
 }
